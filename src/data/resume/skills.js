4933f737349919328674abe449706791a4c74240
Song Zhang <54533402+zszwrsjf@users.noreply.github.com>
2023-06-15 16:55:30 +0900

    Update skills.js

--- src/data/resume/skills.js
+++ src/data/resume/skills.js
@@ -85,8 +85,8 @@ const skills = [
     category: ['Languages', 'Python'],
   },
   {
-    title: 'C++',
-    competency: 2,
+    title: 'C/C++',
+    competency: 3,
     category: ['Languages'],
   },
   {
